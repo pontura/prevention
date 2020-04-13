7eccba4666ff18e161db234f1f209168279aef67
jpamato <juanpamato@gmail.com>
2020-04-13 17:45:47 -0300

    cutscene 1 sfxs

---
+++ prevention/Assets/CutsceneSfx.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneSfx : MonoBehaviour {
+
+	public AudioClip[] clips;
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Play(int i){
+		source.PlayOneShot (clips [i]);
+	}
+}
